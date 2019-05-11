fa152efe3b242f06f9bc55e4cca2bcefd4168a78
Pierre Cavin <tech@sherlox.io>
2019-05-11 17:16:58 +0200

    Update winston-log-json with further OP request

--- answers/winston-log-json/index.js
+++ answers/winston-log-json/index.js
@@ -1,16 +1,29 @@
 const winston = require('winston')
+const { format, transports } = winston
+const path = require('path')
+
+const logFormat = format.printf(info => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`)
 
 const logger = winston.createLogger({
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  format: format.combine(
+    format.label({ label: path.basename(process.mainModule.filename) }),
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.metadata({ fillExcept: ['message', 'level', 'timestamp', 'label'] })
+  ),
   transports: [
-    new winston.transports.Console({
-      name: 'debug-console',
-      level: 'debug',
-      prettyPrint: true,
-      handleExceptions: true,
-      json: false,
-      colorize: true
+    new transports.Console({
+      format: format.combine(
+        format.colorize(),
+        logFormat
+      )
     }),
-    new winston.transports.File({ filename: 'logs/combined.log' })
+    new transports.File({
+      filename: 'logs/combined.log',
+      format: format.combine(
+        format.json()
+      )
+    })
   ],
   exitOnError: false
 })
